da514deb9d2a58dc5e6c84c7615842935971cdb0
aTNa <atay.narynov@gmail.com>
2020-08-22 13:54:42 +0600

    Add file download

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+const queryString = require('query-string');
+
+class App extends Component {
+  state = {
+    mainState: {}
+  }
+
+  componentDidMount() {
+    this.downloadData()
+  }
+
+  downloadData = () => {
+    let urlString =  queryString.parse(window.location.search, {decode: false})
+    console.log(urlString)
+    if (urlString.url) {
+      fetch(urlString.url)
+      .then((response) => {
+        console.log("RESPONSE", response)
+        return response.json();
+    })
+      .then((data) => {
+        console.log("DATA", data);
+        this.setState({mainState: data})
+      });
+    }
+    else {
+      console.log("ERROR: no url detected")
+    }
+  }
+
+  render () {
+    return (
+      <div className="App">
+        {console.log("STATE", this.state.mainState)}
+      </div>
+    );
+  }
 }
 
 export default App;
